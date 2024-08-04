9d2883899880f8d0a53830540593beadeed66303
joycefurtunatobarbosa <joycefurtunatobarbosa@gmail.com>
2024-08-03 23:37:36 -0300

    Inclusão do domínio final nas urls dos servidores

--- frontend/src/server_url.js
+++ frontend/src/server_url.js
@@ -1,9 +1,9 @@
-const server_frontend_url = "http://localhost:8081";
-const server_backend_url = "http://localhost:3000";
-const server_arquivos_url = "http://localhost:3000";
+// const server_frontend_url = "http://localhost:8081";
+// const server_backend_url = "http://localhost:3000";
+// const server_arquivos_url = "http://localhost:3000";
 
-// const server_frontend_url = "http:///www.cenufes.kinghost.net";
-// const server_backend_url = "http:///www.cenufes.kinghost.net/app";
-// const server_arquivos_url = "http://www.cenufes.kinghost.net:21200/";
+const server_frontend_url = "httsp://www.cenufes.com.br";
+const server_backend_url = "http://www.cenufes.com.br/app";
+const server_arquivos_url = "https://www.cenufes.com.br:21200/";
 
 module.exports = { server_frontend_url, server_backend_url, server_arquivos_url };
